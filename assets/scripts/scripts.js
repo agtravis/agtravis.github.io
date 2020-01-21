d92571e531a0d0936b7d194185de04dc6acf3342
George Travis <wewerelikesharks@gmail.com>
2020-01-20 20:31:08 -0800

    js works on about me

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -141,3 +141,54 @@ if (document.getElementById('profile-pic')) {
     }
   });
 }
+
+if (document.getElementById('contact-form')) {
+  var contactForm = document.getElementById('contact-form');
+  var submitButton = document.getElementById('submit');
+  var userName = document.getElementById('username');
+  var email = document.getElementById('email');
+  var message = document.getElementById('message');
+  var isNamed;
+  var isEmailed;
+  var isMessaged;
+
+  submitButton.addEventListener('click', function() {
+    userName.style.border = '';
+    email.style.border = '';
+    message.style.border = '';
+    if (userName.value !== '') {
+      isNamed = true;
+    } else {
+      isNamed = false;
+      // alert('Please enter your name');
+      userName.focus();
+      userName.style.border = '2px solid red';
+    }
+    if (email.value !== '') {
+      if (email.value.indexOf('@') !== -1) {
+        isEmailed = true;
+      } else {
+        isEmailed = false;
+        // alert('Please make sure you enter a valid email address');
+        email.focus();
+        email.style.border = '2px solid red';
+      }
+    } else {
+      isEmailed = false;
+      // alert('Please enter your email');
+      email.focus();
+      email.style.border = '2px solid red';
+    }
+    if (message.value !== '') {
+      isMessaged = true;
+    } else {
+      isMessaged = false;
+      // alert('Please enter a message');
+      message.focus();
+      message.style.border = '2px solid red';
+    }
+    if (isNamed && isEmailed && isMessaged) {
+      contactForm.submit();
+    }
+  });
+}
